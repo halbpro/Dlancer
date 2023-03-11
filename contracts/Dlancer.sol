df57d1be48d9cd5ee27f71d3cead4223d7040f05
Jure Klancar <juresecret@gmail.com>
2023-03-11 21:01:13 +0100

    daily commit

--- contracts/Dlancer.sol
+++ contracts/Dlancer.sol
@@ -4,6 +4,77 @@ pragma solidity ^0.8.9;
 // Uncomment this line to use console.log
 // import "hardhat/console.sol";
 
-contract Dlancer {
-    uint public initialNumber = 1;    
+contract Dlancer {  
+    uint256 offersIndex = 1;
+
+    enum OfferCategory {
+        marketing,
+        sales,
+        development,
+        accounting
+    }
+
+    enum JobStatus {
+        accepted,
+        finished,
+        failed
+    }
+
+    struct Offer {
+        string title;
+        string description;
+        uint256 budget;
+        OfferCategory category;
+        uint256 expirationTime;
+        address client;
+        bool valid;
+        bool inExecution;
+    }
+    struct Job {
+        uint256 id;
+        uint256 offerId;
+        JobStatus status;
+    }
+
+    //client
+    //mapping(address => Offer[]) public offers;
+    mapping(uint256 => Offer) public offers;
+    mapping(address => Job) public jobs;
+
+    event NewOffer(string indexed title, OfferCategory indexed category);
+    event OfferAccepted(address indexed executor, string indexed title);
+
+    constructor () {
+
+    }
+
+    //*******vec klasov, nardimo worker contract, more bit approvan
+
+    function publishOffer(
+        string memory _title,
+        string memory _description,
+        uint256 _budget,
+        OfferCategory _category,
+        uint256 _expirationTime
+    ) external payable {
+        require(msg.value  >= _budget, "Must pay in advance");
+        offers[offersIndex++] = Offer(_title, _description, _budget, _category, _expirationTime, msg.sender, true, false);
+        emit NewOffer(_title, _category);
+    }
+
+    function cancelOffer (uint256 _offerIf) external {
+        Offer memory offer = offers[_offerIf];     
+        require(offer.client == msg.sender, "Only task owner can cancel offer");   
+        require(offer.inExecution == false, "Cannot cancel offer in execution");
+        offers[_offerIf].valid = false;
+    }
+
+    function acceptOffer(uint256 _offerIf) external {
+        Offer memory offer = offers[_offerIf];     
+        require(offer.valid == true, "Offer is no longer valid");
+        require(offer.client != msg.sender, "Cannot accept your own offers");
+        //check expiration date and merge in one modifier
+        offers[_offerIf].inExecution = true;
+        emit OfferAccepted(msg.sender, offer.title);
+    }
 }
--- test/Dlancer.ts
+++ test/Dlancer.ts
@@ -1,18 +1,79 @@
 const { ethers } = require ("hardhat");
 const { expect } = require("chai")
 
-describe("Initial", function () {
-  
+const tokens = (n) => {
+  return ethers.utils.parseUnits(n.toString(), 'ether')
+}
+
+describe("Dlancer", function () {
+  const[TITLE, DESC] = ["offer 1","offer 1 desc"]
+  const [BUDGET, CATEGORY, EXP] = [tokens(1), 2, 100];
+
   let dlancer;
+  let client, executor;
   beforeEach(async () => {
+    [client, executor] = await ethers.getSigners();
+
     const Dlancer = await ethers.getContractFactory("Dlancer");
     dlancer = await Dlancer.deploy();
   });
-
-  it("Should test initial setup", async () => {
-    const initialNumber= await dlancer.initialNumber();
+  describe("Publish offer", () => {
+    let transaction;
+    beforeEach(async() => {
+      transaction = await dlancer.connect(client).publishOffer(
+        TITLE,
+        DESC, 
+        BUDGET,
+        CATEGORY,
+        EXP, { value: BUDGET });
+      await transaction.wait();
+    })
     
-    expect(initialNumber).to.equal(1);
-  });
+    it("Should publish offer", async () => {
+      const offer = await dlancer.offers(1);
+      expect(offer.title).to.equal(TITLE);
+      expect(offer.description).to.equal(DESC);
+      expect(offer.budget).to.equal(BUDGET);
+      expect(offer.category).to.equal(CATEGORY);
+      expect(offer.expirationTime).to.equal(EXP);
+      expect(offer.valid).to.equal(true);
+    });
 
+    it("Cannot publish without sending funds", async() => {
+
+    })
+
+    it("Should emit publish event",async () => {
+      expect(transaction).to.emit(dlancer, "NewOffer")
+    })
+  })
+
+  describe("Cancel offer", () => {
+    let transaction;
+    beforeEach(async() => {
+      transaction = await dlancer.connect(client).publishOffer(
+        TITLE,
+        DESC, 
+        BUDGET,
+        CATEGORY,
+        EXP, { value: BUDGET });
+      await transaction.wait();
+    })
+    
+    it("Should cancel offer", async () => {
+      await dlancer.connect(client).cancelOffer(1);
+      const offer = await dlancer.offers(1);
+      expect(offer.valid).to.equal(false);
+    });
+    
+    it("Other account cannot cancel", async () => {
+      await expect( dlancer.connect(executor).cancelOffer(1)).to.be.revertedWith('Only task owner can cancel offer');
+    });
+
+    it("Cannot cancel offer in execution", async () => {
+      transaction = await dlancer.connect(executor).acceptOffer(1);
+      await expect( dlancer.connect(client).cancelOffer(1)).to.be.revertedWith('Cannot cancel offer in execution');
+    });
+
+  })
 });
